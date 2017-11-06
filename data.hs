007680ee4fd40990febe6334acc7f401f4dc6213
hannessolo <hanneshertach@gmail.com>
2017-11-06 21:44:48 +0000

    rebuild does not work

--- data.hs
+++ data.hs
@@ -2,6 +2,7 @@ module Compression where
 
 import Data.List
 import Data.Char
+import Numeric (showIntAtBase)
 
 data HTree a = Leaf Int a | Node Int (HTree a) (HTree a)
                deriving (Show)
@@ -36,10 +37,6 @@ fig
 
 ----------------------------------------------------------------------------
 
-
-bannana :: Bool
-bannana = otherwise
-
 count :: Eq a => a -> [a] -> Int
 count query list = length [ 1 | x <- list, query == x ]
 
@@ -52,7 +49,7 @@ buildTable list = countAll (nub list) list
 merge :: HTree a -> HTree a -> HTree a
 merge t1 t2
   | t1 < t2   = Node ((freqCount t1) + (freqCount t2)) t1 t2
-  | bannana   = Node ((freqCount t1) + (freqCount t2)) t2 t1
+  | otherwise   = Node ((freqCount t1) + (freqCount t2)) t2 t1
 
 reduce :: [HTree a] -> HTree a
 -- Pre: The argument list non-empty and sorted based on the ordering function
@@ -72,17 +69,18 @@ buildTree list = reduce treeList
 encode :: Eq a => [a] -> HTree a -> Code
 -- Pre: The tree can encode each of the items the list
 encode [] tree     = []
-encode (x:xs) tree = (encode' [] tree) ++ (encode xs tree)
+encode (x:xs) tree = (encode' [] x tree) ++ (encode xs tree)
   where
-    encode' :: Code -> HTree a -> Code
-    encode' acc (Leaf count item)  = acc
-    encode' acc (Leaf count _)     = []
-    encode' acc (Node count t1 t2)
+    encode' :: Eq a => Code -> a -> HTree a -> Code
+    encode' acc x (Leaf count item)
+      | item == x = acc
+      | otherwise = []
+    encode' acc x (Node count t1 t2)
       | null l1 = l2
       | null l2 = l1
       where
-        l1 = encode' (acc ++ [0]) t1
-        l2 = encode' (acc ++ [1]) t2
+        l1 = encode' (acc ++ [0]) x t1
+        l2 = encode' (acc ++ [1]) x t2
 
 decode :: Code -> HTree a -> [a]
 -- Pre: The code is valid with respect to the tree
@@ -97,14 +95,43 @@ decode code tree = decode' code tree
 
 
 compressTree :: HTree Char -> [Int]
-compressTree
-  = undefined
+compressTree (Leaf _ item) = ([1] ++ toBase (ord item) 2 7)
+compressTree (Node _ t1 t2) = [0] ++ (compressTree t1) ++ (compressTree t2)
 
-rebuildTree :: [Int] -> HTree Char
+toBase :: Int -> Int -> Int -> [Int]
+-- Pre: given x, b and n, n is at least as large as the number of digits
+-- required to represent x in base b.
+toBase x b 0 = []
+toBase x b n = q : toBase r b (n - 1)
+  where
+    (q, r) = quotRem x $ pow b (n - 1)
+
+pow :: Int -> Int -> Int
+pow b x = product $ take x $ repeat b
+
 -- Pre: The bitstring ([Int]) is a valid encoding of a Huffman tree
 --      of characters
-rebuildTree
-  = undefined
+rebuildTree :: [Int] -> HTree Char
+-- rebuildTree [] =
+-- rebuildTree (1:ts) = Leaf 0 (binaryTo10 enc)
+--   where
+--     (enc, rest) = splitAt 6 ts
+-- rebuildTree (0:ts) =
+rebuildTree ints = snd (rebuildTree' ints)
+  where
+    rebuildTree' :: [Int] -> (Int, HTree Char)
+    rebuildTree' (0:xs) = (n - 1, Node 0 tr1 tr2)
+      where
+        (_, x2) = splitAt ((length xs) - n) xs
+        (n, tr1) = rebuildTree' xs
+        (_, tr2) = rebuildTree' x2
+    rebuildTree' (1:xs) = (length rest, Leaf 0 (chr ((binaryTo10 enc))))
+      where
+        (enc, rest) = splitAt 6 xs
+
+binaryTo10 :: [Int] -> Int
+binaryTo10 [a] = a
+binaryTo10 xes@(x:xs) = x * (pow 2 ((length xes) - 1)) + binaryTo10 xs
 
 -----------------------------------------------------------------------------
 -- THE SECRET TEST STRING
